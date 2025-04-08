e7100a13e598cffe154f7e5422cad4e5bf7f3ed8
Gal Hermelin <gal.hermelin@gmail.com>
2025-04-08 13:51:09 +0300

    area of a circle

---
+++ Day02/circle.py
@@ -0,0 +1,6 @@
+print("Hello, lets calculate the area of a circle")
+import math
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius ** 2
+print("The area of the circle with radius", radius, "is:", area)
+print("The circumference of the circle with radius", radius, "is:", 2 * math.pi * radius)
