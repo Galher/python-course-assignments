0541139bc5a61a0cee3ac5eb40270fd3037c8d0a
Gal Hermelin <gal.hermelin@gmail.com>
2025-04-23 18:57:39 +0300

    new version

---
+++ Day03/rectangle.py
@@ -0,0 +1,14 @@
+import math
+import sys
+
+if len(sys.argv) != 3:
+    print("please provide the height and width as command line arguments")
+    sys.exit(1)
+
+
+print('Hello, lets calculate the area and the perimeter of a rectangle')
+height = float(sys.argv[1])
+width = float(sys.argv[2])
+area = height * width
+perimeter = 2 * (height + width)      
+print("The area of the rectangle with height", height, "and width", width, "is:", area)
