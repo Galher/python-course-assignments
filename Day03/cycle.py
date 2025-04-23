f6af67a715969808220eafae354aba462262753b
Gal Hermelin <gal.hermelin@gmail.com>
2025-04-23 18:32:26 +0300

    change order

--- Day03/cycle.py
+++ Day03/cycle.py
@@ -1,7 +1,6 @@
 import sys
 import math
 
-print("Hello, lets calculate the area and the circumference of a circle")
 if len(sys.argv) != 2:
     print("please provide the radius as a command line argument")
     sys.exit(1)
@@ -10,6 +9,8 @@ radius = float(sys.argv[1])
 if radius <= 0:
     print("please provide a positive number")
     sys.exit(1)
+
+print("Hello, lets calculate the area and the circumference of a circle")
 area = math.pi * radius ** 2
 print("The area of the circle with radius", radius, "is:", area)
 print("The circumference of the circle with radius", radius, "is:", 2 * math.pi * radius)
