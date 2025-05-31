04b9e8bb3ed47e94dc95be02f3d329db577c8863
Gal Hermelin <gal.hermelin@gmail.com>
2025-05-31 15:11:17 +0300

    Works with a list

--- Day05/count_words_in_list.py
+++ Day05/count_words_in_list.py
@@ -1,14 +1,15 @@
 def count_words_in_list(wlist):
     words_counts = {}
     for word in wlist:
+        word = word.strip()  
         if word not in words_counts:
             words_counts[word] = 1
         else:
             words_counts[word] += 1
     return words_counts
 
-user_input = input("Please enter a list of words separated by commas: ")
-wlist = user_input.split(",")
+user_input = input("Please enter a list of words (e.g. ['apple', 'banana']): ")
+wlist = eval(user_input)
 
 words_counts = count_words_in_list(wlist)
 
